a62aa6bf5f8702e617f35b3116d134c85ab05ab3
Maxxoto <ahmatdanis24@gmail.com>
2019-02-04 19:52:51 +0700

    Git #Init
    Please make a new branch for development purpose instead editing master branch.

---
+++ public/user.js
@@ -0,0 +1,54 @@
+function writeUserData(userId, email) {
+  firebase.database().ref('users/'+userId+"/").set({
+    uid: userId,
+    email: email,
+    point: 10,
+    workshop_count: 0,
+    showcase_count: 0,
+    redeem_history_count: 0,
+  //  email: email,
+   // profile_picture : imageUrl
+  });
+}
+
+function generateTokenWorkshop(){
+    firebase.database().ref('list_token/workshop/').push({
+    event_type: "workshop"
+  //  email: email,
+   // profile_picture : imageUrl
+  });
+}
+function generateTokenShowcase(){
+    firebase.database().ref('list_token/showcase/').push({
+    event_type: "showcase"
+  //  email: email,
+   // profile_picture : imageUrl
+  });
+}
+
+function checkUser(){
+ 
+var userId = firebase.auth().currentUser.uid;
+  // alert(firebase.auth().currentUser.email);
+    checkNull();
+    if(userId != null){
+    //   alert("Exist");
+    }
+    else
+        {
+      //  alert("Null");       
+        }    
+}
+
+function checkNull(){ 
+        var usr = firebase.auth();
+        var uid = usr.currentUser.uid;
+firebase.database().ref('/users/'+uid).once('value').then(function(snapshot) {
+  //  console.log(snapshot.val());
+    if(snapshot.val() != null){
+}
+    else{
+        writeUserData(usr.currentUser.uid, usr.currentUser.email);
+    }
+});    
+}
